524ce4bcbdd9a8f01b3e5a0439637ef9004ad821
Default User <laurence.a.maddox@gmail.com>
2017-09-08 01:43:02 -0500

    added c++ support

--- src/ezfork.h
+++ src/ezfork.h
@@ -1,10 +1,18 @@
 #ifndef _EZFORK_H_
 #define _EZFORK_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <sys/types.h>
 
 int ezfork (
 	int (*childcb)  (void *),        void *childcb_args,
 	int (*parentcb) (pid_t, void *), void *parentcb_args) ;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _EZFORK_H_ */
